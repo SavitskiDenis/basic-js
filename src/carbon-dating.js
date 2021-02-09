71621eaebb609f44b3c96c0f4bb9cf61f60007d3
Denis Savitskiy <denissavitski@strim-tech.com>
2021-02-09 19:52:51 +0300

    feat: add carbon dating implementation

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
+const LN_OF_TWO = 0.693;
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (sampleActivity && typeof sampleActivity === 'string' && /^([1-9]\d*)$/.test(sampleActivity) &&
+    Number.parseInt(sampleActivity) < 9000) {
+    const N = Number.parseInt(sampleActivity);
+    const k = LN_OF_TWO / HALF_LIFE_PERIOD;
+    return Math.log(MODERN_ACTIVITY / N) / k;
+  }
+  return false;
 };

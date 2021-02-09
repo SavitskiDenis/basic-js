03d494024242fd846f96f3109bddc83cd1bbbc0e
Denis Savitskiy <denissavitski@strim-tech.com>
2021-02-09 19:57:10 +0300

    feat: add extended repeater implementation

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const generateAdditionString = function (options) {
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator  = options.additionSeparator  || '|';
+  let result = '';
+  if (options.hasOwnProperty('addition')) {
+    let count = 0;
+    while (count < additionRepeatTimes) {
+      result += options.addition;
+      if (count !== additionRepeatTimes - 1) {
+        result += additionSeparator;
+      }
+      count++;
+    }
+  }
+  return result;
 };
-  
+
+module.exports = function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator  || '+';
+  const additionString = generateAdditionString(options);
+  let result = '';
+  let count = 0;
+  while (count < repeatTimes) {
+    result += str + additionString;
+    if (count !== repeatTimes - 1) {
+      result += separator;
+    }
+    count++;
+  }
+  return result;
+};
